21a74aeadfa49abfd136f0874cafd5bc0bfad3d4
sujitharajendran <sujikavi66@gmail.com>
2017-07-16 12:21:19 +0530

    Add files via upload

---
+++ Sampplehu.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sampplehu {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the size");
+		int n=s.nextInt();
+		int[] d=new int[n];
+		System.out.println("Enter the Array element");
+		int i;
+		for(i=0;i<n;i++){
+			d[i]=s.nextInt();
+		}
+		//Arrays.sort(a);
+		for(i=0;i<n;i++){
+			if(d[i]+d[i+1]>0){
+				System.out.println(d[i]+" "+d[i+1]);
+				break;
+			}
+		}
+		}
+
+	}
+
